104e589bc9bb410176bb912e5c40898cee4d3c89
AntoineSirot <sirot_antoine@bbox.fr>
2023-11-09 22:07:23 +0100

    feature: Using environment variables

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,19 +1,24 @@
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { sepolia, mainnet } from 'wagmi/chains'
+
 import { publicProvider } from 'wagmi/providers/public'
 
-const walletConnectProjectId = '02ebca6a28f82204477f2130ccd0b43f'
+// Take the NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID from .env
+const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || ''
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [mainnet, ...(process.env.NODE_ENV === 'development' ? [sepolia] : [])],
   [
+    // take the NEXT_PUBLIC_ALCHEMY_API_KEY from .env
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "" }),
     publicProvider(),
   ],
 )
 
 const { connectors } = getDefaultWallets({
-  appName: 'My wagmi + RainbowKit App',
+  appName: 'Voting System',
   chains,
   projectId: walletConnectProjectId,
 })
