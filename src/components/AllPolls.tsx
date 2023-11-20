214d61f80681744a0814f878b1a347b0a7686b4e
AntoineSirot <sirot_antoine@bbox.fr>
2023-11-20 16:04:38 +0100

    feat: Filter for active/voted polls

--- src/components/AllPolls.tsx
+++ src/components/AllPolls.tsx
@@ -1,16 +1,17 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Poll } from './Poll';
 import { useContractRead, useAccount } from 'wagmi';
 import { DAOSimulationContract } from './contracts';
 
 export function AllPolls() {
-    const [key, setKey] = useState(0);
-    const [filter, setFilter] = useState('all'); // 'all', 'active', 'past'
     const [polls, setPolls] = useState(0);
+    const [filter, setFilter] = useState('all'); // 'all', 'active', 'past'
     const [showVoted, setShowVoted] = useState('all'); // 'all', 'voted', 'notVoted'
     const [activeToggleActive, setActiveToggleActive] = useState(2); // 1: 'active', 2: 'both', 3: 'inactive'
     const [activeToggleVoted, setActiveToggleVoted] = useState(2); // 1: 'voted', 2: 'all', 3: 'notVoted'
+    const [key, setKey] = useState(false);
+    const [hideAll, setHideAll] = useState(false);
 
     const { address } = useAccount()
 
@@ -20,7 +21,7 @@ export function AllPolls() {
         args: [address]
     });
 
-    const { data: numberOfPolls, isFetched: lengthIsFetched, error: pollsLengthError, isRefetching: loadingRefetch, refetch: refetchAllPools } = useContractRead({
+    const { data: numberOfPolls, isFetched: lengthIsFetched, error: pollsLengthError, isRefetching: loadingRefetch, refetch: refetchAllPolls } = useContractRead({
         ...DAOSimulationContract,
         functionName: 'getSizeOfPolls',
         onSuccess(numberOfPolls) {
@@ -29,9 +30,22 @@ export function AllPolls() {
     });
 
     const handleRefetch = async () => {
-        await refetchAllPools();
-        setKey(key + 1);
+        await refetchAllPolls();
+        await refetchBoolArray();
+        setHideAll(true);
+        handleToggleChangeActive(1);
+        handleToggleChangeVoted(1);
+        setKey(true);
     };
+
+    useEffect(() => {
+        if (key) {
+            handleToggleChangeActive(2);
+            handleToggleChangeVoted(2);
+            setKey(false);
+            setHideAll(false);
+        }
+    }, [key]);
 
     if (!isBoolArrayFetched) {
         return <div>Loading...</div>;
@@ -99,31 +113,35 @@ export function AllPolls() {
         }
     };
 
-    const toggleClassName = "ml-1 mr-1 flex-1 text-center py-2 cursor-pointer transition duration-300";
+    const toggleClassName = "flex-1 text-center py-2 cursor-pointer transition duration-300";
     const activeToggle = "bg-pink-950 text-orange-200";
-    const switchClassName = "flex py-2 justify-between bg-orange-200 text-pink-950 rounded-full mb-4 overflow-hidden";
+    const switchClassName = "flex justify-between bg-orange-200 text-pink-950 rounded-full mb-4 overflow-hidden";
+    const containerStyle = "w-[450px]";
 
     return (
         <div className="text-center">
-            <button onClick={handleRefetch} className={`mb-4 px-4 py-2 rounded-md ${activeToggle} hover:scale-110`}>Refetch</button>
-            <div className="max-w-md mx-auto">
-                <div className={switchClassName}>
-                    <div className={`${toggleClassName} ${activeToggleVoted === 1 ? activeToggle : ''}`} onClick={() => handleToggleChangeVoted(1)}>Already Voted Polls</div>
-                    <div className={`${toggleClassName} ${activeToggleVoted === 2 ? activeToggle : ''}`} onClick={() => handleToggleChangeVoted(2)}>Both</div>
-                    <div className={`${toggleClassName} ${activeToggleVoted === 3 ? activeToggle : ''}`} onClick={() => handleToggleChangeVoted(3)}>Not Voted Polls</div>
+            <button onClick={handleRefetch} className={`mb-4 px-4 py-2 flex-1 rounded-md ${activeToggle} hover:scale-110`}>Refetch</button>
+            {!hideAll && (
+                <div className="max-w-md mx-auto">
+                    <div className={`${containerStyle} ${switchClassName}`}>
+                        <div className={`${toggleClassName} ${activeToggleVoted === 1 ? activeToggle : ''}`} onClick={() => handleToggleChangeVoted(1)}>Already Voted Polls</div>
+                        <div className={`${toggleClassName} ${activeToggleVoted === 2 ? activeToggle : ''}`} onClick={() => handleToggleChangeVoted(2)}>Both</div>
+                        <div className={`${toggleClassName} ${activeToggleVoted === 3 ? activeToggle : ''}`} onClick={() => handleToggleChangeVoted(3)}>Not Voted Polls</div>
+                    </div>
+                    <div className={`${containerStyle} ${switchClassName} mb-8`}>
+                        <div className={`${toggleClassName} ${activeToggleActive === 1 ? activeToggle : ''}`} onClick={() => handleToggleChangeActive(1)}>Active Polls</div>
+                        <div className={`${toggleClassName} ${activeToggleActive === 2 ? activeToggle : ''}`} onClick={() => handleToggleChangeActive(2)}>Both</div>
+                        <div className={`${toggleClassName} ${activeToggleActive === 3 ? activeToggle : ''}`} onClick={() => handleToggleChangeActive(3)}>Past Polls</div>
+                    </div>
                 </div>
-                <div className={switchClassName}>
-                    <div className={`${toggleClassName} ${activeToggleActive === 1 ? activeToggle : ''}`} onClick={() => handleToggleChangeActive(1)}>Active Polls</div>
-                    <div className={`${toggleClassName} ${activeToggleActive === 2 ? activeToggle : ''}`} onClick={() => handleToggleChangeActive(2)}>Both</div>
-                    <div className={`${toggleClassName} ${activeToggleActive === 3 ? activeToggle : ''}`} onClick={() => handleToggleChangeActive(3)}>Past Polls</div>
+            )}
+            {hideAll ? null : (
+                <div className="w-full flex flex-wrap justify-center">
+                    {filteredIndices.map((index) => (
+                        <Poll key={index} index={index} filter={filter} />
+                    ))}
                 </div>
-            </div>
-            {loadingRefetch ? <div>Loading...</div> : null}
-            <div className="w-full flex flex-wrap justify-center">
-                {filteredIndices.map((index) => (
-                    <Poll key={index} index={index} filter={filter} />
-                ))}
-            </div>
+            )}
         </div>
     );
 }
